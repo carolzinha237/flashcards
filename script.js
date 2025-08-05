032e3183a7b64cd49058916dae134525128545d8
carolzinha237 <caroline.vaz.guimaraes@escola.pr.gov.br>
2025-08-05 11:32:22 -0300

    novo

---
+++ script.js
@@ -0,0 +1,16 @@
+
+https://carolzinha237.gith
+
+// Seleciona todos os cartões
+const cartoes = document.querySelectorAll('.cartao');
+
+// Adiciona um evento de clique em cada cartão
+cartoes.forEach(cartao => {
+  cartao.addEventListener('click', () => {
+    // Seleciona o conteúdo interno do cartão
+    const conteudo = cartao.querySelector('.cartao__conteudo');
+   
+    // Alterna a classe 'girado' para aplicar a rotação
+    conteudo.classList.toggle('girado');
+  });
+});
