7143b1a19af4bf3c2040736a8f594d7a9ae73cf9
carolzinha237 <caroline.vaz.guimaraes@escola.pr.gov.br>
2025-08-12 11:03:08 -0300

    novo

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ https://carolzinha237.gith
 
 // Seleciona todos os cartões
 const cartoes = document.querySelectorAll('.cartao');
-async (params) => {
-  
-  
-}// Adiciona um evento de clique em cada cartão
+
+// Adiciona um evento de clique em cada cartão
 cartoes.forEach(cartao => {
   cartao.addEventListener('click', () => {
     // Seleciona o conteúdo interno do cartão
@@ -15,4 +13,4 @@ cartoes.forEach(cartao => {
     // Alterna a classe 'girado' para aplicar a rotação
     conteudo.classList.toggle('girado');
   });
-})
+});
